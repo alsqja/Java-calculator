497826c74204c4be4320203e2450a73f24dfbad6
alsqja <alsqha2626@gmail.com>
2024-10-11 19:44:55 +0900

    Add Lv1 calculator in lv1 package as Calculator1.java

---
+++ src/lv1/Calculator1.java
@@ -0,0 +1,34 @@
+package lv1;
+
+import java.util.Scanner;
+
+public class Calculator1 {
+    public static void main(String[] args) {
+        String operator;
+        do {
+            Scanner sc = new Scanner(System.in);
+            int firstNumber;
+            int secondNumber;
+            System.out.print("첫번째 양의 정수를 입력하세요: ");
+            firstNumber = sc.nextInt();
+
+            System.out.print("두번째 양의 정수를 입력하세요: ");
+            secondNumber = sc.nextInt();
+
+            sc.nextLine();  //  엔터 처리
+
+            System.out.print("연산자를 입력하세요: ");
+            operator = sc.nextLine();
+
+            switch (operator) {
+                case "+" -> System.out.println(firstNumber + secondNumber);
+                case "-" -> System.out.println(firstNumber - secondNumber);
+                case "*" -> System.out.println(firstNumber * secondNumber);
+                case "/" -> System.out.println((double) firstNumber / secondNumber);
+            }
+
+
+        } while (!operator.equals("exit"));
+        System.out.println("프로그램 종료");
+    }
+}
